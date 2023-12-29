c3daa02d16df95c4c63fc2171463629aef831ce2
Angus <155141164+Angus-design@users.noreply.github.com>
2023-12-29 21:26:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 function updateClock() {
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-
-    const hourDeg = (hours % 12 + minutes / 60) * 30;
-    const minuteDeg = (minutes + seconds / 60) * 6;
-    const secondDeg = seconds * 6;
-
-    document.querySelector('.hour-hand').style.transform = `rotate(${hourDeg}deg)`;
-    document.querySelector('.minute-hand').style.transform = `rotate(${minuteDeg}deg)`;
-    document.querySelector('.second-hand').style.transform = `rotate(${secondDeg}deg)`;
-
-    // Update Arabic numerals
-    document.querySelector('.hour-text').textContent = hours % 12 || 12;
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    document.getElementById('clock').textContent = timeString;
 }
 
 // Update the clock every second
